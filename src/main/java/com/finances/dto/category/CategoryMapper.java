b76f6b64f8285203561be22932fc668a2e2d0cd6
RTsoli <robotort2140@gmail.com>
2024-12-03 23:57:09 +0500

    удалил CategoryDTO;

--- src/main/java/com/finances/dto/category/CategoryMapper.java
+++ src/main/java/com/finances/dto/category/CategoryMapper.java
@@ -1,20 +1,10 @@
 package com.finances.dto.category;
 
 import com.finances.model.Category;
-import com.finances.model.Goal;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class CategoryMapper {
-    public static CategoryDTO toGoal(Goal goal) {
-        return new CategoryDTO(goal.getId(),
-                            goal.getOwner().getId(),
-                            goal.getName(),
-                            goal.getAmount(),
-                            goal.getDate(),
-                            goal.getAccount().getId());
-    }
-
     public static CategoryGetResponse toCategoryGetResponse(Category category, long[] idChild) {
         return new CategoryGetResponse(category.getId(),
                                                 idChild,
